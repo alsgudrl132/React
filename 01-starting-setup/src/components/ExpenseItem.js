5ef340dde8da9e764b492fcbc11509f65ef82a89
alsgudrl132 <alsgudrl132@gmail.com>
2023-09-10 23:50:54 +0900

    props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('ko-KR', { month: 'long' });
+    const day = props.date.toLocaleString('ko-KR', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
     return (
-        <div>
-            <div>March 28th 2021</div>
+        <div className="expense-item">
             <div>
-                <h2>Car Insurance</h2>
-                <div>$294.57</div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
