cc31085f9ed20981767c3f1cfd2d519b0b722e79
alsgudrl132 <alsgudrl132@gmail.com>
2023-09-14 11:13:18 +0900

    task

---
+++ 01-task/src/components/Concepts.js
@@ -0,0 +1,10 @@
+const Concepts = (props) => {
+    return (
+        <li className="concept">
+            <img src={props.image} alt="TODO: TITLE" />
+            <h2>TODO: {props.title}</h2>
+            <p>TODO: {props.description}</p>
+        </li>
+    );
+};
+export default Concepts;
