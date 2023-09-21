9a23879944d7d491f045033aa02343a7f8682044
alsgudrl132 <alsgudrl132@gmail.com>
2023-09-21 14:43:52 +0900

    useEffect

--- nomad/react-for-beginners/src/App.js
+++ nomad/react-for-beginners/src/App.js
@@ -1,11 +1,21 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    function Hello() {
+        const byeFn = () => console.log('bye');
+        const hiFn = () => {
+            console.log('created');
+            return byeFn;
+        };
+        useEffect(hiFn, []);
+        return <h1>Hello</h1>;
+    }
     return (
-        <div className="App">
-            <h1 className={styles.title}>Welcome back</h1>
-            <Button text={'Continue'} />
+        <div>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
         </div>
     );
 }
